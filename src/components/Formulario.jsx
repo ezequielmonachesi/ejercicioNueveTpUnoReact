5ff9c109063c4009fa6e074760b00abad3a031d9
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-06-01 01:30:28 -0300

    se empieza a maquetar

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,17 @@
+import { Button } from "react-bootstrap";
+
+const Formulario = () => {
+    return (
+        <>
+        <Card.Header>Llenar formulario para crear cita</Card.Header>
+        <Card.Body>
+
+        </Card.Body>
+        <Card.Footer>
+            <Button className="">Enviar</Button>
+        </Card.Footer>
+        </>
+    );
+};
+
+export default Formulario;
